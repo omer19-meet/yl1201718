b45a6bce786ae5644a0b79b5fbb0567caebbe946
laith19-meet <laith19@meet.mit.edu>
2017-10-29 18:51:47 +0200

    my first commit

---
+++ lab1.py
@@ -0,0 +1,10 @@
+number1 = 345
+print(number1)
+number2 = number1//2
+print(number2)
+
+nums = [3,2,3,4,5,6]
+x = 0
+for n in range(10):
+	print(nums[x])
+	x+=1
---
+++ lab2.py
@@ -0,0 +1,26 @@
+einav = [20,40,60,69]
+
+def lsup():
+	print(einav[-1], einav[0])
+
+
+
+gilaad = [1.66, 2, 3, 5, 4.55,50,3.33 0, 10,20,16,27062002]
+omer = []
+
+def lessthan5_():
+	for i in gilaad:
+		if (i < 5) :
+			print(i)
+
+
+def new_ls():
+	for i in gilaad:
+		if (i < 5) :
+			omer.append(i)
+	print(omer)
+
+
+
+new_ls()
+
